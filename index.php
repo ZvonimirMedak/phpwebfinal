bd6fd684578daad3bc60d26603a55b5a2462b96b
Zvonimir <zvonimir.medak@hotmail.com>
2021-06-14 14:42:56 +0200

    php

--- index.php
+++ index.php
@@ -52,9 +52,9 @@ $cleardb_server = $cleardb_url["host"];
 $cleardb_username = $cleardb_url["user"];
 $cleardb_password = $cleardb_url["pass"];
 $cleardb_db = substr($cleardb_url["path"],1);
-echo $cleardb_url["user"]
-echo $cleardb_url["host"]
-echo $cleardb_url["pass"]
+//echo $cleardb_url["user"]
+//echo $cleardb_url["host"]
+//echo $cleardb_url["pass"]
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
